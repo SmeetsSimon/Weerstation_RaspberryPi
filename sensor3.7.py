d49d1c1e66520e10cad191b51b1723df5829fd59
SmeetsSimon <77274583+SmeetsSimon@users.noreply.github.com>
2022-06-11 17:41:18 +0200

    commentaar

--- sensor3.7.py
+++ sensor3.7.py
@@ -1,14 +1,16 @@
+#libraries nodig voor sensor 
 import board
 import busio
 from adafruit_bme280 import basic as adafruit_bme280
 import time
 
-i2c = busio.I2C(board.SCL, board.SDA)
+i2c = busio.I2C(board.SCL, board.SDA) #uitlezen van de SCL en SDA
 bme280 = adafruit_bme280.Adafruit_BME280_I2C(i2c)
 
 
 while True:
-    print(f"\n{'Temperature:':<13} {round(bme280.temperature, 1)} °C")
+    #alle gegevens
+    print(f"\n{'Temperature:':<13} {round(bme280.temperature, 1)} °C") 
     print(f"{'Humidity:':<13} {round(bme280.humidity, 1)} %")
     print(f"{'Pressure:':<13} {round(bme280.pressure, 1)} hPa")
     time.sleep(5)
