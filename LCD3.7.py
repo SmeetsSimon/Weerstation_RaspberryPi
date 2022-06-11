4c9b181a490af9549b35d189083ea893da19d2f3
SmeetsSimon <77274583+SmeetsSimon@users.noreply.github.com>
2022-06-11 17:44:01 +0200

    commentaar

--- LCD3.7.py
+++ LCD3.7.py
@@ -1,16 +1,14 @@
+#libraries nodig voor LCD
 from adafruit_bme280 import basic as adafruit_bme280
 from rpi_lcd import LCD
 
-
-lcd = LCD()
+lcd = LCD() 
 
 try:
     while True:
-
         lcd.text("regel 1", 1)
         lcd.text("regel 2", 2)
         lcd.text("regel 3", 3)
         lcd.text("regel 4" , 4)
-
 except KeyboardInterrupt:
     lcd.clear()
